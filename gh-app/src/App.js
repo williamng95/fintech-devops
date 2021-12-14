972905845c16b10a291b0a4ad11bad1ced1dad52
William Ng <84236898+williamng95@users.noreply.github.com>
2021-12-14 11:29:16 +0800

    test reuse call

--- gh-app/src/App.js
+++ gh-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          can both workflows launch correctly?
+          can i reuse workflows?
         </p>
         <a
           className="App-link"
