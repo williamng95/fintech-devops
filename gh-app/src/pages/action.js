9eab5d20baea2b67069f3cca649b9e1a28fe2e06
William Ng <84236898+williamng95@users.noreply.github.com>
2021-12-17 00:22:44 +0800

    for direct main merge

---
+++ gh-app/src/pages/action.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import Page from '../templates/page'
+
+const Link = () => {
+    return (
+        <Page>
+            READY ACTION!!
+        </Page>)
+}
+
+export default Action
